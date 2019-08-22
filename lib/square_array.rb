b5412ee8f1a22ff3285b0bcd102ee8f114684fcd
Aniket B <aniket.b@outlook.com>
2019-08-22 09:28:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,20 +1,3 @@
-#def square_array(array)
- # counter = 0
- # new_arr = []
- 
-#  while counter < array.length do
-    #new_arr.push(array.each ** 2)
-   # counter += 1
-  #end
- # return new_arr
-#end
-
-    #array.each do |element|
-   #     new_array << element ** 2
-  #  end
- #   new_array
-#end
-
 def square_array(array)
   new_array = []
   e = 0
@@ -25,11 +8,3 @@ def square_array(array)
   end
   new_array
 end
-
-
-#def square_array(array)
-#    new_array = []
-#    array.each { |e| new_array << e ** 2 }
-#    new_array
-#end
-    
